ebd163f47b11241119aaaaff11116e62cb959c71
berru-g <g.leberruyer@gmail.com>
2023-07-05 23:15:54 +0200

    timeline

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
 
-/*
 $(window).on("scroll", function() {
-    $(".scroll-section").each(function() {
-        if (isElementInViewport(this)) {
-            $(this).addClass("visible");
-        }
-    });
-});
-
-function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-
-*/
-$(window).on("scroll", function () {
-  $(".scroll-section").each(function () {
+  $(".scroll-section").each(function() {
     if (isElementInViewport(this)) {
       $(this).addClass("visible");
       if ($(this).hasClass("scroll-1")) {
@@ -75,3 +56,29 @@ hamburgerMenu.addEventListener('click', () => {
   });
 });
 
+var items = document.querySelectorAll(".timeline li");
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function callbackFunc() {
+  for (var i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      if(!items[i].classList.contains("in-view")){
+        items[i].classList.add("in-view");
+      }
+    } else if(items[i].classList.contains("in-view")) {
+        items[i].classList.remove("in-view");
+    }
+  }
+}
+ 
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
